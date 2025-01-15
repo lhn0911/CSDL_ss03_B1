49a7083486cddb29493740c9722d08e3a2fa6f7c
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2025-01-15 11:13:58 +0700

    Add files via upload

---
+++ Bai1.sql
@@ -0,0 +1,10 @@
+create table SinhVien(
+MaSV int primary key,
+TenSV varchar(20),
+Tuoi int
+);
+insert into SinhVien(MaSV,TenSV,Tuoi) 
+values
+(1,'Nguyen Anh Tu',20),
+(2,'Tran Minh Hoang',22),
+(3,'Pham Thi Mai',21)
