999816f006e633f06895b8cfbbd71515df06c39b
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2025-01-16 09:53:57 +0700

    Add files via upload

--- Bai1.sql
+++ Bai1.sql
@@ -1,10 +1,14 @@
-create table SinhVien(
-MaSV int primary key,
-TenSV varchar(20),
-Tuoi int
+create table Student (
+Student_id  int primary key not null,
+Student_name varchar(255) not null,
+age int check (age>=18) not null,
+gender varchar(10) check(gender in ('male','female','other')) not null,
+registration_date datetime default current_timestamp not null
 );
-insert into SinhVien(MaSV,TenSV,Tuoi) 
+insert into Student (Student_id,Student_name,age,gender,registration_date) 
 values
-(1,'Nguyen Anh Tu',20),
-(2,'Tran Minh Hoang',22),
-(3,'Pham Thi Mai',21)
+(1, 'Nguyễn Văn A', 20, 'Male', '2025-01-15 08:30:00'),
+(2, 'Trần Thị B', 22, 'Female', '2025-01-14 09:00:00'),
+(3, 'Lê Minh C', 19, 'Male', '2025-01-13 10:15:00'),
+(4, 'Phan Thị D', 21, 'Female', '2025-01-12 11:20:00'),
+(5, 'Hoàng Văn E', 23, 'Other', '2025-01-11 14:30:00');
